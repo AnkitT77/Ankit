701953c21d2510f93754bb6f918df17748091113
ankittiwari <ankitester28@gmail.com>
2022-03-30 17:28:33 +0530

    changes

--- src/footer.js
+++ src/footer.js
@@ -15,39 +15,39 @@ export default function Footer() {
 				id="contact"
 			>
 				<div className="flex gap-x-3">
-					<a href="https://github.com/dharmik-18">
+					<a href="https://github.com/AnkitT77">
 						<div className="p-3 rounded-full flex justify-center items-center bg-back border border-gray-700 hover:bg-nav transform hover:scale-110">
 							<FaGithub size={25} color={"white"} />
 						</div>
 					</a>
-					<a href="https://www.linkedin.com/in/dharmik-rathod/">
+					<a href="https://www.linkedin.com/mwlite/in/ankit-tiwari-1b087210/">
 						<div className="p-3 rounded-full flex justify-center items-center bg-back border border-gray-700 hover:bg-blue-700 transform hover:scale-110">
 							<FaLinkedinIn size={25} color={"white"} />
 						</div>
 					</a>
-					<a href="https://www.hackerrank.com/Dharmik91">
+					{/* <a href="https://www.hackerrank.com/Dharmik91">
 						<div className="p-3 rounded-full flex justify-center items-center bg-back border border-gray-700 hover:bg-green-500 transform hover:scale-110">
 							<FaHackerrank size={25} color={"white"} />
 						</div>
-					</a>
+					</a> */}
 
-					<a href="https://www.instagram.com/rathoddharmik18/">
+					<a href="https://www.instagram.com/__ankittiwari_/">
 						<div className="p-3 rounded-full flex justify-center items-center bg-back border border-gray-700 hover:bg-pink-500 transform hover:scale-110">
 							<FaInstagram size={25} color={"white"} />
 						</div>
 					</a>
-					<a href="https://wa.me/919967950259">
+					<a href="https://wa.me/918652740476">
 						<div className="p-3 rounded-full flex justify-center items-center bg-back border border-gray-700 hover:bg-green-600 transform hover:scale-110">
 							<FaWhatsapp size={25} color={"white"} />
 						</div>
 					</a>
-					<a href="mailto:rathoddharmik9@gmail.com?subject=Mail from Our Site">
+					<a href="mailto:ankit.807744@gmail.com?subject=Mail from Our Site">
 						<div className="p-3 rounded-full flex justify-center items-center bg-back border border-gray-700 hover:bg-red-500 transform hover:scale-110">
 							<SiGmail size={25} color={"white"} />
 						</div>
 					</a>
 				</div>
-				<p className="text-gray-400">Designed and developed by Dharmik</p>
+				<p className="text-gray-400">Designed and developed by Ankit</p>
 			</div>
 		);
 }
